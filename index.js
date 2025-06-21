e37aef8e56291b75c690b5907e70aec1bc7f9d62
Chukstev <anowaichukwuemeka@gmail.com>
2025-06-21 14:28:09 +0100

    added comments

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
+// target the toggle div
 const toggle = document.querySelectorAll(".toggle");
 
+// get the individual toggles
 toggle.forEach((item) => {
+  // add a click event to each toggle
   item.addEventListener("click", () => {
-  const parent = item.parentElement;
-  const plusIcon = item.querySelector(".plus");
-  const minusIcon = item.querySelector(".minus");
-parent.classList.toggle("active")
-if (parent.classList.contains("active")) {
-    plusIcon.style.display = "none";
-    minusIcon.style.display = "block";
-} else {
-    plusIcon.style.display = "block";
-    minusIcon.style.display = "none";
-}
+    // this targets the list element
+    const parent = item.parentElement;
+    //   get the plus icon
+    const plusIcon = item.querySelector(".plus");
+    //   get the minus icon
+    const minusIcon = item.querySelector(".minus");
+    //   set the list to be active on toggle
+    parent.classList.toggle("active");
+    if (parent.classList.contains("active")) {
+      // what happens when the toggle is active or not is determined by this line
+      plusIcon.style.display = "none";
+      minusIcon.style.display = "block";
+    } else {
+      plusIcon.style.display = "block";
+      minusIcon.style.display = "none";
+    }
   });
 });
+ 
